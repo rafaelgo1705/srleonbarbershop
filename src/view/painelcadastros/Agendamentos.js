3b2d1fd5bf31d762f22d26850f806dac814d072d
rafaelgo1705 <rr147440@gmail.com>
2019-11-14 16:55:44 -0300

    v1.0.2.8 - Agendamentos

--- src/view/painelcadastros/Agendamentos.js
+++ src/view/painelcadastros/Agendamentos.js
@@ -9,18 +9,21 @@ import { ScrollView } from 'react-native-gesture-handler';
 
 import Base64 from '../../base64/Base64';
 
+import moment from 'moment';
+import DatePicker from 'react-native-datepicker';
+
 export default class Agendamentos extends React.Component {
     constructor(props){
       super(props);
 
       this.state = {
         arrayAgendamentos: [],
-    
+        date: moment(new Date()),
       }
     }
 
     componentDidMount() {
-        this.carregarListaCabeleireiros()
+        this.carregarAgendamentos()
         this.backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
             this.props.navigation.navigate('Cadastramento');
             return true;
@@ -31,27 +34,58 @@ export default class Agendamentos extends React.Component {
         this.backHandler.remove();
     }
 
-    carregarListaCabeleireiros = () => {
+    selecionarDiaCalendario = (date) => {
+      this.setState({date:date})
+      this.carregarAgendamentos();
+    }
+
+    carregarAgendamentos = () => {
         var ref = database().ref("/leonbarbershop/agendamentos");
-        this.state.arrayAgendamentos = []
+        this.setState({arrayAgendamentos:[]})
+
+        var data = new Date(this.state.date)
+
+        var diaAtual = data.getDate();
+        var mesAtual = data.getMonth()+1;
+        var anoAtual = data.getFullYear();
+
+        console.log("Dia: "+ diaAtual + " | Mês: "+ mesAtual+ " | Ano: " + anoAtual)
+        console.log("Data: "+ data)
+
+        var horaAtual = moment(new Date().getHours())
+        var minutosAtuais = moment(new Date().getMinutes())
     
-        ref.orderByChild("ano").on("child_added", (snapshot) => {
-            this.setState({ 
-                arrayAgendamentos : 
-                [...this.state.arrayAgendamentos, ...[
-                    {
-                    id:snapshot.key,
-                    nomeCliente:snapshot.child("nomeCliente").val(),
-                    preco:snapshot.child("precoCorte").val(),
-                    hora: snapshot.child("hora").val(),
-                    minuto: snapshot.child("minuto").val(),
-                    dia: snapshot.child("dia").val(),
-                    mes: snapshot.child("mes").val(),
-                    ano: snapshot.child("ano").val(),
-                    nomeCorte: snapshot.child("nomeCorte").val(),
-                    }
-                ]] 
-            })
+        ref.orderByChild("hora").on("child_added", (snapshot) => {
+          var dia = 0, mes = 0, ano = 0, hora = 0, minutos = 0;
+
+          dia = snapshot.child("dia").val();
+          mes = snapshot.child("mes").val();
+          ano = snapshot.child("ano").val();
+          hora = snapshot.child("hora").val();
+          minutos = snapshot.child("minuto").val();
+
+          if(ano == anoAtual){
+            if(mes == mesAtual){
+              if(dia == diaAtual){
+                this.setState({ 
+                  arrayAgendamentos : 
+                  [...this.state.arrayAgendamentos, ...[
+                      {
+                      id:snapshot.key,
+                      nomeCliente:snapshot.child("nomeCliente").val(),
+                      preco:snapshot.child("precoCorte").val(),
+                      hora: hora,
+                      minuto: minutos,
+                      dia: dia,
+                      mes: mes,
+                      ano: ano,
+                      nomeCorte: snapshot.child("nomeCorte").val(),
+                      }
+                  ]] 
+                })
+              }
+            }
+          }
             
         })
     }
@@ -59,6 +93,33 @@ export default class Agendamentos extends React.Component {
     render() {
         return (
             <View style={estilos.viewTabs}>
+              <View style={estilos.viewSuperiorStatus}>
+                <DatePicker
+                  style={{width: 200}}
+                  date={this.state.date}
+                  mode="date"
+                  format="DD-MM-YYYY"
+                  confirmBtnText="Selecionar"
+                  cancelBtnText="Cancelar"
+                  customStyles={{ 
+                    dateInput: {
+                      marginLeft: 36,
+                      
+                    },
+                    dateIcon: {
+                      position: 'absolute',
+                      left: 0,
+                      top: 4,
+                      marginLeft: 0,
+                      
+                    },
+                    dateText: {
+                      color: "#FFFFFF"
+                    }
+                  }}
+                  onDateChange={(dateNot, dateYes) => {this.selecionarDiaCalendario(dateYes)}}/>
+              </View>
+
               <ScrollView contentContainerStyle={{flexGrow: 1}}>
               <FlatList
                 data={this.state.arrayAgendamentos}
